8f5b0b3334e286ac45513fa05f9127e4766ac65e
Magdalena Ledezma <Magdalena Ledezma>
2017-04-18 13:49:06 -0400

    Created Puzzle class, made variables, constructors, and an Array List

--- src/Model/Puzzle.java
+++ src/Model/Puzzle.java
@@ -1,4 +1,7 @@
 package Model;
+
+import java.util.ArrayList;
+
 /**
  * Class: Puzzle
  * 
@@ -13,4 +16,30 @@ package Model;
  */
 public class Puzzle {
 
-}
+//ID-int , description-string, item- item.itemID, 
+	static int puzzleID;
+	static String description;
+	
+	protected Puzzle(int puzzleID, String description){
+		this.puzzleID = puzzleID;
+		this.description = description;
+		
+	}
+	
+	protected static ArrayList<Puzzle> puzzle = new ArrayList<Puzzle>();
+	{
+		//for IDs put in string
+		int PZ_01 = 0;
+		Puzzle puzzle1 = new Puzzle(PZ_01, "You need to Turn Back on the Lights and Turn back on the Environmental Controls By Flipping Switches so you can navigate the ship.");
+		int PZ_02 = 0;
+		Puzzle puzzle2 = new Puzzle(PZ_02, "The User Needs To find I_7 In RM_07 to enter in RM_19 to Enter RM_20. This allows the user to be on step closer to Finishing the game.");
+		int PZ_03 = 0;
+		Puzzle puzzle3 = new Puzzle(PZ_03, "You Need to Find the Space suit in RM_21 So you can Survive Walking through RM_13 That is exposed to the harsh environments of space.");
+		int PZ_04 = 0;
+		Puzzle puzzle4 = new Puzzle(PZ_04, "You Need To Kill M_7 in RM_25 With W_4 Found in Rm_26. M7 Will not die until You Can use W_4 on it. Once complete you are able to Proceed");
+		int PZ_05 = 0;
+		Puzzle puzzle5 = new Puzzle(PZ_05, "You Need to find I_9 in RM_30 So that You Can Activate the Elevator To enter the Bridge.");
+	}
+	}
+	
+
