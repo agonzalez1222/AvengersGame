63a22af8d1a5abc7a3f4da85873791386be531ae
apancha <apanchatcharam@ggc.edu>
2017-04-21 03:17:09 -0400

    added roommaker (doesnt work) and map classes

--- src/Model/RoomMaker.java
+++ src/Model/RoomMaker.java
@@ -1,5 +1,41 @@
 package Model;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 public class RoomMaker {
+	
+		private final Map<Integer, Map<Integer, Room>> map = (Map<Integer, Map<Integer, Room>>) new HashMap<Integer, Map<Integer, Room>>();
+	    private Room currentRoom;
+	    private int currentX = 0;
+	    private int currentY = 0;
+	    
+	    private RoomMaker(){
+	    }
+	/*    private void putRoom(int x, int y, Room room) {
+	        if (!map.containsKey(x)) {
+	            map.put(x, new HashMap<Integer, Room>());
+	        }
+	        map.get(x).put(y, room);
+	    }
 
+	    private Room getRoom(int x, int y) {
+	        return map.get(x).get(y);
+	    }
+
+	    private boolean roomExists(int x, int y) {
+	        if (!map.containsKey(x)) {
+	            return false;
+	        }
+	        return map.get(x).containsKey(y);
+	    }
+
+	    private boolean isComplete() {
+	        return currentRoom.isBossRoom() && currentRoom.isComplete();
+	    }
+
+*/
 }
