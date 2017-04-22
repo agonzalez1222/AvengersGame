2379f4d2bb89aa7ae2f4df4f0a545793ad4b0c6b
apancha <apanchatcharam@ggc.edu>
2017-04-22 01:23:23 -0400

    fixed room stuff still needs work but no errors anymore

--- src/Model/RoomMaker.java
+++ src/Model/RoomMaker.java
@@ -1,21 +1,27 @@
 package Model;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
-import com.sun.javafx.collections.MappingChange.Map;
+
+//import com.sun.javafx.collections.MappingChange.Map;
 
 public class RoomMaker {
 	
-		private final Map<Integer, Map<Integer, Room>> map = (Map<Integer, Map<Integer, Room>>) new HashMap<Integer, Map<Integer, Room>>();
+	    private final Map<Integer, Map<Integer, Room>> map = new HashMap<Integer, Map<Integer, Room>>();
+	    //HashMap<Int,Gmap> map =new HashMap<Int,Gmap>();
 	    private Room currentRoom;
 	    private int currentX = 0;
 	    private int currentY = 0;
 	    
 	    private RoomMaker(){
 	    }
-	/*    private void putRoom(int x, int y, Room room) {
+	    private void putRoom(int x, int y, Room room) {
 	        if (!map.containsKey(x)) {
 	            map.put(x, new HashMap<Integer, Room>());
 	        }
@@ -33,9 +39,70 @@ public class RoomMaker {
 	        return map.get(x).containsKey(y);
 	    }
 
-	    private boolean isComplete() {
-	        return currentRoom.isBossRoom() && currentRoom.isComplete();
+//	    private boolean isComplete() {
+//	        return currentRoom.isBossRoom() && currentRoom.isComplete();
+//	    }
+
+
+	    public void movePlayer(Player player) throws IOException {
+	        boolean northPossible = roomExists(currentX, currentY + 1);
+	        boolean southPossible = roomExists(currentX, currentY - 1);
+	        boolean eastPossible = roomExists(currentX + 1, currentY);
+	        boolean westPossible = roomExists(currentX - 1, currentY);
+	        System.out.print("Where would you like to go :");
+	        if (northPossible) {
+	            System.out.print(" North (n)");
+	        }
+	        if (eastPossible) {
+	            System.out.print(" East (e)");
+	        }
+	        if (southPossible) {
+	            System.out.print(" South (s)");
+	        }
+	        if (westPossible) {
+	            System.out.print(" West (w)");
+	        }
+	        System.out.print(" ? ");
+	        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	        String direction = in.readLine();
+	        if (direction.equals("n") && northPossible) {
+	            currentY++;
+	        } else if (direction.equals("s") && southPossible) {
+	            currentY--;
+	        } else if (direction.equals("e") && eastPossible) {
+	            currentX++;
+	        } else if (direction.equals("w") && westPossible) {
+	            currentX--;
+	        }
+	        currentRoom = getRoom(currentX, currentY);
+	        currentRoom.enter(player);
 	    }
 
-*/
-}
+//	    public void startQuest(Player player) throws IOException {
+//	        while (player.isAlive() && !isComplete()) {
+//	            movePlayer(player);
+//	        }
+//	        if (player.isAlive()) {
+//	            System.out.println(Art.CROWN);
+//	        } else {
+//	            System.out.println(Art.REAPER);
+//	        }
+//	    }
+
+	    public static RoomMaker newInstance() {
+	    	RoomMaker roomG = new RoomMaker();
+	    	roomG.putRoom(0, 0, Room.newRegularInstance());
+	    	roomG.putRoom(-1, 1, Room.newRegularInstance());
+	    	roomG.putRoom(0, 1, Room.newRegularInstance());
+	    	roomG.putRoom(1, 1, Room.newRegularInstance());
+	    	roomG.putRoom(-1, 2, Room.newRegularInstance());
+	    	roomG.putRoom(1, 2, Room.newRegularInstance());
+	    	roomG.putRoom(-1, 3, Room.newRegularInstance());
+	    	roomG.putRoom(0, 3, Room.newRegularInstance());
+	    	roomG.putRoom(1, 3, Room.newRegularInstance());
+	       // dungeon.putRoom(0, 4, Room.newBossInstance());
+	    	roomG.currentRoom = roomG.getRoom(0, 0);
+	        return roomG;
+	    }
+
+	}
