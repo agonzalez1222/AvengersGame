6f1fe937c1d11cb92fda6bce8659813a66c4e840
John P <apanchatcharam@ggc.edu>
2017-04-24 06:23:50 -0400

    gametester to view swap (help us)

--- src/Model/GameTester.java
+++ src/Model/GameTester.java
@@ -46,48 +46,48 @@ public class GameTester extends Component {
        }
     }
  
-    public static void main(String[] args) {
-    	Game.intro();
-      /*  JFrame f = new JFrame("Game Map");
-             
-        f.addWindowListener(new WindowAdapter(){
-                public void windowClosing(WindowEvent e) {
-                    System.exit(0);
-                }
-            });
- 
-        f.add(new GameTester());
-        f.pack();
-        f.setVisible(true);
-    }
-    */
-    //when map is typed it displays it 
-    	Items test = new Items();
-	//	Items test1 = new Items();
-    	
-    	Player p1 = new Player();
-    	try
-    	{
-    		
-        	RoomMaker bs = new RoomMaker();
-        	final int WIDTH = 40;
-            final int HEIGHT = 40;
-            Room[][] room = new Room[WIDTH][HEIGHT];
-            bs.build(room, WIDTH, HEIGHT);
-            System.out.println();
-            bs.print(room, bs.getX(), bs.getY());
-            
-            bs.movePlayer(p1);
-            bs.print(room, bs.getX(), bs.getY());
-    		
-    		
-    		
-    	}
-    	catch(IOException e)
-    	{
-    		System.out.println("cant move");
-    	}
-		
+   public static void main(String[] args) {
+//    	Game.intro();
+//      /*  JFrame f = new JFrame("Game Map");
+//             
+//        f.addWindowListener(new WindowAdapter(){
+//                public void windowClosing(WindowEvent e) {
+//                    System.exit(0);
+//                }
+//            });
+// 
+//        f.add(new GameTester());
+//        f.pack();
+//        f.setVisible(true);
+//    }
+//    */
+//    //when map is typed it displays it 
+//    	Items test = new Items();
+//	//	Items test1 = new Items();
+//    	
+//    	Player p1 = new Player();
+//    	try
+//    	{
+//    		
+//        	RoomMaker bs = new RoomMaker();
+//        	final int WIDTH = 40;
+//            final int HEIGHT = 40;
+//            Room[][] room = new Room[WIDTH][HEIGHT];
+//            bs.build(room, WIDTH, HEIGHT);
+//            System.out.println();
+//            bs.print(room, bs.getX(), bs.getY());
+//            
+//            bs.movePlayer(p1);
+//            bs.print(room, bs.getX(), bs.getY());
+//    		
+//    		
+//    		
+//    	}
+//    	catch(IOException e)
+//    	{
+//    		System.out.println("cant move");
+//    	}
+//		
         
         
         
